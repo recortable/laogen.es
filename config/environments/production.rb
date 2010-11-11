83d940a496204a78cbb12a1c70efa2bb0654873e
danigb <danigb@gmail.com>
2010-11-11 23:19:28 +0100

    mail exceptions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 
 # Cache your content for a longer time, the default is 5.minutes
 # config.after_initialize do 
